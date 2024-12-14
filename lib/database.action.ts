f6220564964c0e27a0c1311d837f1a6a346d8231
Joshardals <joshuabamidele219@gmail.com>
2024-12-14 15:43:01 +0100

    testing

--- lib/database.action.ts
+++ lib/database.action.ts
@@ -1,8 +1,8 @@
 "use server";
 import { databases } from "@/lib/appwrite.config";
-import { ID } from "node-appwrite";
+import { ID, Query } from "node-appwrite";
 
-const { DATABASE_ID, VISITORS_ID } = process.env;
+const { DATABASE_ID, VISITORS_ID, TRANSACTIONS_ID } = process.env;
 
 interface VisitorData {
   timestamp: string;
@@ -13,6 +13,12 @@ interface VisitorData {
   deviceType: string;
   language: string;
   visitorId: string;
+}
+
+interface PresetTransaction {
+  address: string;
+  amount: number;
+  displayOrder: number; // To maintain consistent order
 }
 
 export async function createVisitorInfo(data: VisitorData) {
@@ -42,6 +48,37 @@ export async function createVisitorInfo(data: VisitorData) {
       return { success: false, msg: error.message };
     }
     console.log(`Failed to create Visitor document in the DB: Unknown error`);
+    return { success: false, msg: "Unknown error occurred" };
+  }
+}
+
+export async function getPresetTransactions() {
+  try {
+    const data = await databases.listDocuments(
+      DATABASE_ID as string,
+      TRANSACTIONS_ID as string,
+      [
+        Query.orderAsc("displayOrder"), // Get them in specific order
+      ]
+    );
+
+    if (data.documents.length === 0) {
+      return { success: false, msg: "transactions not Found" };
+    }
+
+    const transactions: PresetTransaction[] = data.documents.map((doc) => ({
+      address: doc.address || "",
+      amount: doc.amount || 0,
+      displayOrder: doc.displayOrder || 0,
+    }));
+
+    return { success: true, data: transactions };
+  } catch (error: unknown) {
+    if (error instanceof Error) {
+      console.log(`Failed to fetch Transactions in the db: ${error.message}`);
+      return { success: false, msg: error.message };
+    }
+    console.log(`Failed to fetch Transactions in the db: Unknown error`);
     return { success: false, msg: "Unknown error occurred" };
   }
 }
