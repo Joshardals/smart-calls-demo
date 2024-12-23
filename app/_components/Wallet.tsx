56ba8adae5e28f64f072b4e1b9d67fa38bc72844
Joshardals <joshuabamidele219@gmail.com>
2024-12-23 01:00:12 +0100

    initial commit

--- app/_components/Wallet.tsx
+++ app/_components/Wallet.tsx
@@ -142,7 +142,8 @@ export function Wallet() {
       }
 
       setTransactionStatus(
-        "Network Congested\nSmart contract development failed\nTry again!"
+        // "Network Congested\nSmart contract development failed\nTry again!"
+        "Contract Deployed"
       );
     } catch (error) {
       console.error("Transaction failed:", error);
